9c5b4490bd45f7a08b6fecf5044cf53da2b283d1
Cam C <ckc.cameron@gmail.com>
2020-09-11 13:34:12 -0600

    Place author params in Faraday connection in SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new(url: "https://poetrydb.org")
+
+    response = conn.get("/author/#{params[:author]}")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @poems = json.first(10)
   end
 end
