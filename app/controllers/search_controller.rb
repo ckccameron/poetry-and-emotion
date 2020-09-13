f6867d39ee6f11213dd4e8cd58a56d93d2074094
Cam C <ckc.cameron@gmail.com>
2020-09-12 20:03:55 -0600

    Refactor poems variable to call from SearchFacade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,5 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://poetrydb.org")
-
-    response = conn.get("/author/#{params[:author]}")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    @poems = json.first(10)
-
-    conn2 = Faraday.new(url: ENV['IBM_API_URL']) do |connection|
-      connection.basic_auth("apikey", ENV['IBM_API_KEY'])
-    end
-
-    response2 = conn.get("v3/tone?version=2017-09-21&text=#{poem.lines}")
-
-    json2 = JSON.parse(response.body, symbolize_names: true)
+    @poems = SearchFacade.author_name_first_ten_poem_results(params[:author])
   end
 end
