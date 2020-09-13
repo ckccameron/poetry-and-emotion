97abc68f8b2405ad9a02a7ccfc5afcd2346d400f
Cam C <ckc.cameron@gmail.com>
2020-09-12 19:25:49 -0600

    Add tone analyzer connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,5 +7,13 @@ class SearchController < ApplicationController
     json = JSON.parse(response.body, symbolize_names: true)
 
     @poems = json.first(10)
+
+    conn2 = Faraday.new(url: ENV['IBM_API_URL']) do |connection|
+      connection.basic_auth("apikey", ENV['IBM_API_KEY'])
+    end
+
+    response2 = conn.get("v3/tone?version=2017-09-21&text=#{poem.lines}")
+
+    json2 = JSON.parse(response.body, symbolize_names: true)
   end
 end
